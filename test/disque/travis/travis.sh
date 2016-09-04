a329198523731d238e806327142bcaadbbac7e83
Evan Huang <evanhuang8@gmail.com>
2016-09-04 11:35:39 -0500

    Add travis script

---
+++ test/disque/travis/travis.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+DQPATH=${TRAVIS_BUILD_DIR}/tests/disque/travis
+
+# Install disque
+
+mkdir -p ${DQPATH}/src
+/usr/bin/env git clone https://github.com/antirez/disque.git ${DQPATH}/src/disque
+cd ${DQPATH}/src/disque/src
+make
+
+# Remove existing files
+
+rm ${DQPATH}/nodes-*.conf
+
+# Run 3 instances
+
+${DQPATH}/src/disque/src/disque-server ${DQPATH}/node1.conf
+${DQPATH}/src/disque/src/disque-server ${DQPATH}/node2.conf
+${DQPATH}/src/disque/src/disque-server ${DQPATH}/node3.conf
+
+# Wait & join
+
+sleep 5
+${DQPATH}/src/disque/src/disque -p 7711 cluster meet 127.0.0.1 7712
+${DQPATH}/src/disque/src/disque -p 7711 cluster meet 127.0.0.1 7713
